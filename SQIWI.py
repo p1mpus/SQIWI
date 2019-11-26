233cc35ed3bd1cdb831f69a1216db16fb1876569
p1mpus <45327810+p1mpus@users.noreply.github.com>
2019-11-26 10:50:58 +0300

    Add files via upload

---
+++ SQIWI.py
@@ -0,0 +1,28 @@
+from SimpleQIWI import *
+import time, os
+
+token1 = "8ffcd1d00b270bdcf8b00fcbd67a9d4f"
+phone1 = "+79043370635"
+phone = "-"
+
+os.system('clear')
+print("<//LYNEX TEAM//>")
+print("[SQIWI / coded by p1mpus]")
+
+token = input("Тoken QIWI: ")
+api = QApi(token=token, phone=phone)
+print("\nBalance QIWI: ")
+print(api.balance)
+
+a = input("\nQIWI account for transfer (phone number): ")
+s = input("Amount of money to transfer: ")
+c = input("Comment for transfer: ")
+print("\nMoney transfer to your QIWI...")
+api.pay(account=a, amount=s, comment=c)
+time.sleep(5)
+print("Money transfer to your QIWI [OK]")
+print("Successfully!")
+print("You Took: " + s + " Rubles")
+print("Balance QIWI: ")
+print(api.balance)
+input("\nEnter...")
