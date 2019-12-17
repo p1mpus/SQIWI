82bd5d5c858ba14a5db791a3cbe88366909197ea
p1mpus <45327810+p1mpus@users.noreply.github.com>
2019-12-17 12:05:19 +0300

    Add files via upload

---
+++ Errors.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class QIWIAPIError(Exception):
+    pass
+
+
+class ArgumentError(Exception):
+    pass
+
+
+class InvalidTokenError(Exception):
+    pass
+
+
+class OverridingEx(Exception):
+    pass
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class QIWIAPIError(Exception):
+    pass
+
+
+class ArgumentError(Exception):
+    pass
+
+
+class InvalidTokenError(Exception):
+    pass
+
+
+class OverridingEx(Exception):
+    pass
