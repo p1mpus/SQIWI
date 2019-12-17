a39c6120e66444fd58deae27dbd5c3b465071fc0
p1mpus <45327810+p1mpus@users.noreply.github.com>
2019-12-17 12:06:12 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2017 SimpleQIWI
+# Written by Ember
+# https://github.com/Emberium/SimpleQIWI
+
+from SimpleQIWI.Errors import *
+from SimpleQIWI.Session import *
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2017 SimpleQIWI
+# Written by Ember
+# https://github.com/Emberium/SimpleQIWI
+
+from SimpleQIWI.Errors import *
+from SimpleQIWI.Session import *
